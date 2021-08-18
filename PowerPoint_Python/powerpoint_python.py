a9e70e44b44dee75715b98b75f7bae104c20ce6e
Christian Freitas <62871678+ChristianSF@users.noreply.github.com>
2021-08-18 17:47:15 -0300

    Add files via upload

--- PowerPoint_Python/powerpoint_python.py
+++ PowerPoint_Python/powerpoint_python.py
@@ -10,6 +10,7 @@ Original file is located at
 !pip install python-pptx
 
 from pptx import Presentation
+from pptx.util import Inches
 
 prs = Presentation()
 title_slide_layout = prs.slide_layouts[0]
@@ -21,4 +22,22 @@ subtitle = slide.placeholders[1]
 title.text = "Hello, World!"
 subtitle.text = "Teste python Power Point"
 
-prs.save('teste.pptx')
+slide1_register = prs.slide_layouts[1]
+
+slide1 = prs.slides.add_slide(slide1_register)
+
+title1 = slide1.shapes.title
+title1.text = "Novo título teste"
+
+slide2_register = prs.slide_layouts[2]
+slide2 = prs.slides.add_slide(slide2_register)
+title2 = slide2.shapes.title
+title2.text = "Teste com imagem"
+
+img = "/content/img_power_point.png"
+
+from_left = Inches(3)
+from_top = Inches(2)
+adiciona_img = slide2.shapes.add_picture(img, from_left, from_top)
+
+prs.save('teste_imagem.pptx')
